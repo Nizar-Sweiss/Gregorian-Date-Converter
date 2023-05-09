d2c303e5dc500def3d7a3616afa86a0b9428f4ac
nizar sweiss <92578643+Nizar-Sweiss@users.noreply.github.com>
2023-05-09 14:27:03 +0300

    feat: Convert the selected date to hijri Fucntion

--- app/lib/Presentation/date_convertor_class.dart
+++ app/lib/Presentation/date_convertor_class.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:http/http.dart' as http;
 
 class DateConvertor extends StatefulWidget {
   const DateConvertor({super.key});
@@ -12,6 +15,7 @@ class _DateConvertorState extends State<DateConvertor> {
   DateTime today = DateTime.now();
   DateTime firstDay = DateTime.utc(1900, 1, 1);
   DateTime lastDay = DateTime.utc(2040, 1, 1);
+  String DateTest = "ccc";
   void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
       today = day;
@@ -33,8 +37,42 @@ class _DateConvertorState extends State<DateConvertor> {
           lastDay: lastDay,
           onDaySelected: _onDaySelected,
         ),
-        Text("Selected Date : ${today.toString().split(" ")[0]} ")
+        Text("Selected Date : ${today.toString().split(" ")[0]} "),
+        TextButton(
+            onPressed: () async {
+              String gregorianDate = dateFormater();
+              if (gregorianDate.isNotEmpty) {
+                // Call API to convert date
+                String hijriDate = await convertDate(gregorianDate);
+                setState(() {
+                  DateTest = hijriDate;
+                });
+              }
+            },
+            child: Text(DateTest))
       ])),
     );
+  }
+
+  Future<String> convertDate(String gregorianDate) async {
+    final url = Uri.parse('http://api.aladhan.com/v1/gToH/$gregorianDate');
+    final response = await http.get(url);
+    // print("Method res : ${response.body}");
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+      return jsonResponse['data']['hijri']['date'];
+    } else {
+      throw Exception('Failed to convert date');
+    }
+  }
+
+  String dateFormater() {
+    String dateString = today.toString().split(" ")[0];
+    DateFormat inputFormat = DateFormat('yyyy-MM-dd');
+    DateTime date = inputFormat.parse(dateString);
+    var formate1 = "${date.day}-${date.month}-${date.year}";
+    print(formate1);
+
+    return formate1; // Output: Mon, May 9, 2023
   }
 }
